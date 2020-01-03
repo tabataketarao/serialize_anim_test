2ce496e3d03fb0d381a8a06a256acce743eceb79
tabataketarao <tabataketarao@gmail.com>
2020-01-03 14:12:04 +0900

    Initial commit

---
+++ Assets/Scripts/SerializeAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SerializeAnim : MonoBehaviour
+{
+    Animator mAnimator;
+    [SerializeField] public bool BoolWait;
+    [SerializeField] public bool BoolStay;
+    [SerializeField] public bool BoolIn;
+    [SerializeField] public bool BoolOut;
+    [SerializeField] public bool BoolRotate;
+
+    void Awake()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        mAnimator.SetBool("wait", BoolWait);
+        mAnimator.SetBool("in", BoolIn);
+        mAnimator.SetBool("out", BoolOut);
+        mAnimator.SetBool("rotate", BoolRotate);
+    }
+}
---
+++ Assets/Scripts/SerializeAnimTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SerializeAnimTest : MonoBehaviour
+{
+    Animator mAnimator;
+    private void Awake()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,10, 150, 30), "in"))
+        {
+            mAnimator.SetTrigger("in");
+        }
+        if (GUI.Button(new Rect(10, 40, 150, 30), "out"))
+        {
+            mAnimator.SetTrigger("out");
+        }
+    }
+}
